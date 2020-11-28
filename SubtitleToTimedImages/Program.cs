450b7dcbaafe8b33dcc88ddcf2fd21f8d6b112c1
Erwin de Haan <EraYaN@users.noreply.github.com>
2020-11-28 17:34:02 +0100

    Reenable all examples

--- SubtitleToTimedImages/Program.cs
+++ SubtitleToTimedImages/Program.cs
@@ -7,8 +7,7 @@ namespace SubtitleToTimedImages
 {
     class Program
     {
-
-        const string MainPath = @"E:\";
+        const string MainPath = @"E:\subtitle_parsing_examples";
 
         static void Main(string[] args)
         {
@@ -19,52 +18,52 @@ namespace SubtitleToTimedImages
             };
 
             Stopwatch sw = new Stopwatch();
-            //sw.Start();
-            //var events = SubtitleUtil.ReadMatroskaVobSub(Path.Combine(MainPath,"out.mkv"),0);
-            //sw.Stop();
-            //Console.WriteLine("Parsed Matroska VobSub in {0} ms for {1} items", sw.ElapsedMilliseconds, events.Count);
+            sw.Start();
+            var events = SubtitleUtil.ReadMatroskaVobSub(Path.Combine(MainPath, "vobsub_example.mkv"), 0); //special subtitle only matroska files so track 0
+            sw.Stop();
+            Console.WriteLine("Parsed Matroska VobSub in {0} ms for {1} items", sw.ElapsedMilliseconds, events.Count);
 
-            //sw.Restart();
-            //using (StreamWriter file = File.CreateText(Path.Combine(MainPath, "out_mkv.json")))
-            //{
-            //    serializer.Serialize(file, events);
-            //}
-            //sw.Stop();
-            //Console.WriteLine("Saved Matroska VobSub JSON in {0} ms", sw.ElapsedMilliseconds);
+            sw.Restart();
+            using (StreamWriter file = File.CreateText(Path.Combine(MainPath, "vobsub_example_out.json")))
+            {
+                serializer.Serialize(file, events);
+            }
+            sw.Stop();
+            Console.WriteLine("Saved Matroska VobSub JSON in {0} ms", sw.ElapsedMilliseconds);
 
-            //sw.Restart();
-            var events = SubtitleUtil.ReadMatroskaBluraySup(Path.Combine(MainPath, "eng.mkv"), 0);
+            sw.Restart();
+            events = SubtitleUtil.ReadMatroskaBluraySup(Path.Combine(MainPath, "bluraysup_example.mkv"), 0); //special subtitle only matroska files so track 0
             sw.Stop();
             Console.WriteLine("Parsed Matroska Sup in {0} ms for {1} items", sw.ElapsedMilliseconds, events.Count);
 
             sw.Restart();
-            using (StreamWriter file = File.CreateText(Path.Combine(MainPath, "eng_mkv.json")))
+            using (StreamWriter file = File.CreateText(Path.Combine(MainPath, "bluraysup_example_out.json")))
             {
                 serializer.Serialize(file, events);
             }
             sw.Stop();
             Console.WriteLine("Saved Matroska Sup JSON in {0} ms", sw.ElapsedMilliseconds);
 
-            //sw.Restart();            
-            //events = SubtitleUtil.ReadVobSub(Path.Combine(MainPath, "out.sub"), Path.Combine(MainPath, "out.idx"));
-            //sw.Stop();
-            //Console.WriteLine("Parsed VobSub in {0} ms for {1} items", sw.ElapsedMilliseconds, events.Count);
-
-            //sw.Restart();
-            //using (StreamWriter file = File.CreateText(Path.Combine(MainPath, "out.json")))
-            //{
-            //    serializer.Serialize(file, events);
-            //}
-            //sw.Stop();
-            //Console.WriteLine("Saved VobSub JSON in {0} ms", sw.ElapsedMilliseconds);
+            sw.Restart();
+            events = SubtitleUtil.ReadVobSub(Path.Combine(MainPath, "vobsub_example.sub"), Path.Combine(MainPath, "vobsub_example.idx"));
+            sw.Stop();
+            Console.WriteLine("Parsed VobSub in {0} ms for {1} items", sw.ElapsedMilliseconds, events.Count);
 
             sw.Restart();
-            events = SubtitleUtil.ReadSup(Path.Combine(MainPath, "eng.sup"));
+            using (StreamWriter file = File.CreateText(Path.Combine(MainPath, "vobsub_example_out2.json")))
+            {
+                serializer.Serialize(file, events);
+            }
+            sw.Stop();
+            Console.WriteLine("Saved VobSub JSON in {0} ms", sw.ElapsedMilliseconds);
+
+            sw.Restart();
+            events = SubtitleUtil.ReadSup(Path.Combine(MainPath, "bluraysup_example.sup"));
             sw.Stop();
             Console.WriteLine("Parsed Sup in {0} ms for {1} items", sw.ElapsedMilliseconds, events.Count);
 
             sw.Restart();
-            using (StreamWriter file = File.CreateText(Path.Combine(MainPath, "eng.json")))
+            using (StreamWriter file = File.CreateText(Path.Combine(MainPath, "bluraysup_example_out2.json")))
             {                
                 serializer.Serialize(file, events);
             }
